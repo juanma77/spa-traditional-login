179d5652fbffc2f16c9afe476531637754085e8f
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-07-23 16:44:13 -0500

    binding user model properties to input fields in user login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from '../../models/user.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public user: UserModel;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.user = new UserModel();
+
+  }
+
+  public onSubmit( form: NgForm ) {
+
+    if( form.invalid ) {
+
+      return; 
+
+    }
+
+    console.log(' Form enviado ');
+
+    console.log( this.user );
+    console.log( form ); 
+
+
   }
 
 }
